5d5fadf70c6b99eb586c171c76380587e003e083
adam.schnaare@gmail.com <ba7O&GJzv%EmL&26>
2015-08-22 16:04:59 -0500

    beta test commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,68 +1,78 @@
 (function () {
-    'use strict';
-    module.exports = function (grunt) {
-        // show elapsed time at the end
-        require('time-grunt')(grunt);
-        // load all grunt tasks
-        grunt.loadNpmTasks('grunt-githooks');
-        require('load-grunt-tasks')(grunt);
+  'use strict';
+  module.exports = function (grunt) {
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-        // configurable paths
-        var yeomanConfig = {
-            src: 'src',
-            dist: 'dist',
-            temp: '.tmp'
-        };
-
-        grunt.initConfig({
-            yeoman: yeomanConfig,
-            pkg: grunt.file.readJSON('package.json'),
-            githooks: {
-              all: {
-                // Will run the jshint and test:unit tasks at every commit
-                'pre-commit': 'jshint'
-              }
-            },
-            clean: {
-                all: ['<%= yeoman.temp %>', '<%= yeoman.dist %>/*']
-            },
-            //
-            jshint: {
-                options: {
-                    jshintrc: '.jshintrc',
-                    reporter: require('jshint-stylish')
-                },
-                all: [
-                    '<%= yeoman.src %>/*.js'
-                ]
-            },
-            // Minification settings
-            uglify: {
-                options: {
-                    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */'
-                }, dist: {
-                    files: [
-                        {
-                            expand: true,
-                            cwd: '<%= yeoman.src %>',
-                            src: '**/*.js',
-                            dest: '<%= yeoman.dist %>',
-                            ext: '.min.js'
-                        }
-                    ]
-                }
-            }
-        });
-
-        // Default Tasks
-        grunt.registerTask('hint', [
-            'jshint',
-            'clean:all',
-            'uglify'
-        ]);
-        grunt.registerTask('default', [
-            'clean:all',
-            'uglify'
-        ]);
+    // configurable paths
+    var yeomanConfig = {
+      src: 'src',
+      dist: 'dist',
+      temp: '.tmp'
     };
+
+    grunt.initConfig({
+      yeoman: yeomanConfig, pkg: grunt.file.readJSON('package.json'),
+      githooks: {
+        all: {
+          // Will run the jshint and test:unit tasks at every commit
+          'pre-commit': 'precommit'
+        }
+      },
+      gitcommit: {
+        task: {
+          options: {
+            message: 'Auto build commit',
+            noVerify: true,
+            noStatus: false
+          },
+          files: {
+            src: ['<%= yeoman.dist %>/**']
+          }
+        }
+      },
+      clean: {
+        all: ['<%= yeoman.temp %>']
+      }, //
+      jshint: {
+        options: {
+          jshintrc: '.jshintrc', reporter: require('jshint-stylish')
+        },
+        all: [
+          '<%= yeoman.src %>/*.js']
+      }, // Minification settings
+      uglify: {
+        options: {
+          banner: '/*! ' +
+          '<%= pkg.name %> - ' +
+          'v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd, h:MM:ss TT Z") %> - ' +
+          '*/'
+        },
+        dist: {
+          files: [
+            {expand: true, cwd: '<%= yeoman.src %>', src: '**/*.js', dest: '<%= yeoman.dist %>', ext: '.min.js'}
+          ]
+        }
+      }
+    });
+
+    // Default Tasks
+    grunt.registerTask('hint', [
+      'jshint',
+      'clean:all',
+      'uglify'
+    ]);
+    grunt.registerTask('precommit', [
+      'clean:all',
+      'uglify'
+    ]);
+    grunt.registerTask('default', [
+      'githooks',
+      'clean:all',
+      'uglify'
+    ]);
+  };
 }());
